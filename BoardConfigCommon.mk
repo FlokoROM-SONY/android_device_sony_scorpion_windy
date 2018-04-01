0f683d91fc3008edd829476fb57bdf06fc4fb99c
Kousuke Nagatsuka <koron393@gmail.com>
2018-04-02 03:19:22 +0900

    scorpion-common: Revert "Remove unneeded sepolicies for Oreo"
    * Re-add sysfs sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,6 +26,10 @@ TARGET_RECOVERY_DEVICE_DIRS := device/sony/scorpion_windy
 BOARD_VOLD_MAX_PARTITIONS := 26
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12253589504 # 12253605888 - 16384
 
+# SELinux
+BOARD_SEPOLICY_DIRS += device/sony/scorpion_windy/sepolicy
+
+# DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
 
 # Audio
