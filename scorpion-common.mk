6e30b78458e8be5751350dd5e5b15c33de3ba868
koron393 <koron393@gmail.com>
2018-12-13 22:34:54 +0900

    scorpion-common: Revert to stock 23.5.A.1.291 mixer_paths
    * Revert mixer_paths.xml
    * Add mixer_paths_auxpcm.xml

--- scorpion-common.mk
+++ scorpion-common.mk
@@ -29,6 +29,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/vendor/etc/thermanager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/thermanager.xml \
     $(LOCAL_PATH)/rootdir/system/vendor/etc/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/rootdir/system/vendor/etc/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/rootdir/system/vendor/etc/mixer_paths_auxpcm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_auxpcm.xml \
     $(LOCAL_PATH)/rootdir/system/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/rootdir/system/vendor/etc/audio_policy_volumes_drc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes_drc.xml \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
