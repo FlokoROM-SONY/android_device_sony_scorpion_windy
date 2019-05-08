73442d41410eda825726bf9031b1e6d326bfbd3f
koron393 <koron393@gmail.com>
2019-05-08 17:28:35 +0900

    scorpion_windy: Ready for lineage-16.0

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aicp_scorpion_windy.mk
+    $(LOCAL_DIR)/lineage_scorpion_windy.mk
---
+++ lineage_scorpion_windy.mk
@@ -0,0 +1,24 @@
+# Platform path
+PLATFORM_COMMON_PATH := device/sony/shinano-common
+
+# Release name
+PRODUCT_RELEASE_NAME := scorpion_windy
+
+# Inherit LOS common Phone stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+$(call inherit-product, device/sony/scorpion_windy/full_scorpion_windy.mk)
+
+# build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="SGP611-user 6.0.1 23.5.A.1.291 713254892 release-keys"
+
+BUILD_FINGERPRINT := \
+    "Sony/SGP611/SGP611:6.0.1/23.5.A.1.291/713254892:user/release-keys"
+
+PRODUCT_NAME := lineage_scorpion_windy
+PRODUCT_DEVICE := scorpion_windy
+
+# Resolution
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 1920
